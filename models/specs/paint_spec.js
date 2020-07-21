dfdef74343912c6b78e32d1d50107cb93682d900
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-07-21 15:57:05 +0100

    paint isEmpty method passes tests

--- models/specs/paint_spec.js
+++ models/specs/paint_spec.js
@@ -3,13 +3,22 @@ const Paint = require("../paint")
 
 describe('Paint', function() {
     let paint;
+    let paint2;
 
     beforeEach(function () {
         paint = new Paint(10);
+        paint2 = new Paint(0);
     });
     
     it('should have a number of litres of paint', function(){
         assert.strictEqual(10, paint.litres);
     });
 
+    it('should be able to check if it is empty (not empty)', function () {
+        assert.strictEqual(false, paint.isEmpty());
+    });
+
+    it('should be able to check if it is empty (is empty)', function () {
+        assert.strictEqual(true, paint2.isEmpty());
+    });
 });
