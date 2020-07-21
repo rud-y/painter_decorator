f0fe23860353bfa7f0e53e1bec5a204fc6db33b2
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-07-21 16:18:48 +0100

    decorator addPaint method passes test

--- models/decorator.js
+++ models/decorator.js
@@ -2,4 +2,8 @@ const Decorator = function () {
     this.stock = [];
 };
 
+Decorator.prototype.addPaint = function (paint) {
+    this.stock.push(paint);
+};
+
 module.exports = Decorator;
--- models/specs/decorator_spec.js
+++ models/specs/decorator_spec.js
@@ -1,14 +1,22 @@
-const assert = require("assert")
-const Decorator = require("../decorator")
+const assert = require("assert");
+const Decorator = require("../decorator");
+const Paint = require("../paint");
 
 describe("Decorator", function () {
     let decorator;
+    let paint;
 
     beforeEach( function () {
         decorator = new Decorator();
+        paint = new Paint(50);
     });
 
     it("should start with an empty paint stock", function () {
         assert.deepStrictEqual([], decorator.stock);
     });
+
+    it("should be able to add a paint can to stock", function () {
+        decorator.addPaint(paint);
+        assert.deepStrictEqual([paint], decorator.stock);
+    })
 });
